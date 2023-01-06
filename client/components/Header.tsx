ed7851f5d78956d0584f2b46836396143b424e9f
holliskuang <kuanghollis@gmail.com>
2023-01-06 03:26:47 -0800

    Logo Added

--- client/components/Header.tsx
+++ client/components/Header.tsx
@@ -4,19 +4,9 @@ export default function Header() {
       <div className="ui-layout-container">
         <div className="ui-section-header__layout ui-layout-flex">
           {/*<-- LOGO -->*/}
+
           <a href="#" role="link" aria-label="#">
-            <svg
-              viewBox="0 0 18 18"
-              height="18"
-              width="18"
-              role="img"
-              aria-label="#"
-            >
-              <path
-                fill="#353535"
-                d="M0 0h4.5v9a4.5 4.5 0 109 0V6H18v3A9 9 0 110 9V0zm18 4.5V0h-4.5v4.5H18z"
-              />
-            </svg>
+            <img src="https://res.cloudinary.com/dd97ovnmi/image/upload/v1673004204/letter-l_pcq0m6.png" height="80px" width="80px" loading="lazy"/>
           </a>
           {/*<!-- MENU --> */}
           <a
