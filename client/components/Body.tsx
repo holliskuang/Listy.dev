bcd3df883fc52f3a33f2cfb2201259d76a7dae18
holliskuang <kuanghollis@gmail.com>
2023-01-06 17:12:12 -0800

    Landing Page Done

--- client/components/Body.tsx
+++ client/components/Body.tsx
@@ -8,8 +8,34 @@ export default function Body(props: any) {
           {/*-- COPYWRITING -*/}
           <div className="ui-layout-column-6 ui-layout-column-center">
             <FunText />
-           
-            {/*-- CTA -*/}
+            <div className="imageHolder">
+              <img
+                src="https://res.cloudinary.com/dd97ovnmi/image/upload/v1673052458/old_school_love_2_oeqwd8.png"
+                height="250px"
+                width="250px"
+                className="hvr-grow"
+              />
+              <img
+                src="https://res.cloudinary.com/dd97ovnmi/image/upload/v1673052453/old_school_love-4_1_s1aafn.png"
+                height="250px"
+                width="250px"
+                className="hvr-grow"
+              />
+              <img
+                src="
+                https://res.cloudinary.com/dd97ovnmi/image/upload/v1673052449/old_school_love-2_1_cjnyno.png"
+                height="250px"
+                width="250px"
+                className="hvr-grow"
+              />
+              <img
+                src="
+                https://res.cloudinary.com/dd97ovnmi/image/upload/v1673052443/old_school_love-3_1_kourc1.png"
+                height="250px"
+                width="250px"
+                className="hvr-grow"
+              />
+            </div>
             <div className="ui-component-cta ui-layout-flex">
               <a
                 onClick={props.spotifyLogIn}
@@ -22,7 +48,6 @@ export default function Body(props: any) {
               </a>
             </div>
           </div>
-          {/*-- IMAGE -*/}
         </div>
       </section>
     </main>
